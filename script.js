ed6fd410da213218289f9eddcfe74c657f77d2e1
MurakamiTaku <124675365+MurakamiTaku@users.noreply.github.com>
2023-04-04 00:25:00 +0900

    なぞなぞver4

--- script.js
+++ script.js
@@ -5,20 +5,22 @@ const result = document.getElementById("result");
 const go = document.getElementById("go");
 
 hintBtn1.addEventListener("click", function () {
-  result1.innerText = "時計は何回り？";
+  result1.innerText = "時計の針=回転する";
 });
 hintBtn2.addEventListener("click", function () {
   result2.innerText = "デジタルの数字も見てみよう";
 });
 hintBtn3.addEventListener("click", function () {
-  result3.innerText = "時計回りにデジタル数字を足していくと、、、";
+  result3.innerText =
+    "時計回り、または反時計回りに回転させてデジタル数字を足していくと、、、";
 });
 
 checkBtn.addEventListener("click", function () {
   const answer = answerInput.value.toLowerCase();
 
   if (answer === "さん") {
-    go.innerText = "正解!時計回りでデジタル数字を足すと33になります。";
+    go.innerText =
+      "正解! 正方形を90度ずつ回転しながらデジタル数字を足すと33になります。";
   } else {
     go.innerText = "不正解!";
   }
